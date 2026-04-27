25088fbd7bfc8f245d5a35f5607b9f0d581c64ea
maddiescoutsowell-maker <maddiescoutsowell@gmail.com>
2026-04-26 21:30:50 -0400

    Add files via upload
    Added C11 Cookie Sales assignment

---
+++ Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub SalesBindingNavigatorSaveItem_Click(sender As Object, e As EventArgs) Handles SalesBindingNavigatorSaveItem.Click
+        Try
+            Me.Validate()
+            Me.SalesBindingSource.EndEdit()
+            Me.TableAdapterManager.UpdateAll(Me.CookiesDataSet)
+        Catch ex As Exception
+            MessageBox.Show("Error saving data: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'CookiesDataSet.Sales' table. You can move, or remove it, as needed.
+        Me.SalesTableAdapter.Fill(Me.CookiesDataSet.Sales)
+
+    End Sub
+End Class
